4f8c368343a130e6696947fac7cd0fc8088781e9
0Day–At0mik <46066196+MaDatomiKK@users.noreply.github.com>
2019-02-02 14:16:48 +0100

    Delete INSTALL.sh

--- INSTALL.sh
+++
@@ -1,8 +0,0 @@
-apt-get install figlet
-apt-get install lolcat
-apt-get install mdk3
-apt-get install aircrack-ng
-apt-get install git
-git clone https://github.com/s0md3v/XSStrike/
-apt-get install xterm
-apt-get install links
